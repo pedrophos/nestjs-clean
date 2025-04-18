b72c5ac04cdbd6da80cfc021fd1ed75a999e7ecb
Pedro Henrique <pedrohenriquedarck@hotmnail.com>
2025-04-18 18:18:02 -0300

    Feat: Usert token decorator

--- src/controllers/create-question.controler.ts
+++ src/controllers/create-question.controler.ts
@@ -1,7 +1,8 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { UserPayload } from 'src/auth/jwt.strategy'
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
@@ -9,7 +10,7 @@ export class CreateQuestionteController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle() {
-    return 'ok'
+  async handle(@CurrentUser() user: UserPayload) {
+    return user.sub
   }
 }
