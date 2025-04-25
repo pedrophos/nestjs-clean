4d2ace0da9abba1fda8918b63f076598fc1982a8
Pedro Henrique <pedrohenriquedarck@hotmnail.com>
2025-04-24 21:24:09 -0300

    Feat: Fetch recent questions

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionteController } from './controllers/create-question.controler'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { CreateQuestionteController } from './controllers/create-question.contro
     CreateAccountController,
     AuthenticateController,
     CreateQuestionteController,
+    FetchRecentQuestionsController,
   ],
   providers: [PrismaService],
 })
